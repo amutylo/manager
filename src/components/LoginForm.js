4ee067e2cc167f6d09db5347b8e441ab30bc578f
Andrii Mutylo <amutylo@gmail.com>
2018-02-05 22:14:38 +0200

    added Redux-Thunk, firebase auth, async call redux

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from "../actions";
+import { emailChanged, passwordChanged, loginUser } from "../actions";
 import { Card, CardSection, Input, Button } from './common';
+
 
 
 class LoginForm extends Component {
@@ -11,6 +12,12 @@ class LoginForm extends Component {
 
   onPasswordChanged(text) {
     this.props.passwordChanged(text)
+  }
+
+  onButtonPress() {
+    const {email, password} = this.props;
+
+    this.props.loginUser({email, password});
   }
 
   render(){
@@ -34,7 +41,7 @@ class LoginForm extends Component {
           />
         </CardSection>
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
@@ -50,4 +57,6 @@ const mapStateToProps = state =>{
   };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser
+})(LoginForm);
