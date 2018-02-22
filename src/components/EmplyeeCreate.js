b4e4b6a7d53333863ed5b8bc30213aa2cd51640e
Andrii Mutylo <amutylo@gmail.com>
2018-02-22 23:08:35 +0200

    save user to firebase

--- src/components/EmplyeeCreate.js
+++ src/components/EmplyeeCreate.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from "./common";
 
 
 class EmplyeeCreate extends Component {
+  onButtonPress() {
+    const {name, phone, shift} = this.props;
+
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' })
+  }
+
   render() {
     return (
       <Card>
@@ -41,7 +47,9 @@ class EmplyeeCreate extends Component {
           </Picker>
         </CardSection>
         <CardSection>
-          <Button>Create</Button>
+          <Button
+            onPress={this.onButtonPress.bind(this)}
+          >Create</Button>
         </CardSection>
       </Card>
     );
@@ -62,4 +70,7 @@ const mapStateToProps = (state) => {
 };
 
 
-export default connect(mapStateToProps, { employeeUpdate })(EmplyeeCreate);
+export default connect(mapStateToProps, {
+  employeeUpdate,
+  employeeCreate
+})(EmplyeeCreate);
